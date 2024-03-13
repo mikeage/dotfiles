9f2d1e538cf7f1416a7744384e01028570c818bf
Mike Miller <github@mikeage.net>
2024-03-13 08:05:05 +0200

    Add nvim-cmp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -54,6 +54,13 @@ Plug('stevearc/conform.nvim')                                             -- For
 Plug('github/copilot.vim')                                                -- AI enhanced coding
 Plug('mbbill/undotree')                                                   -- Undo tree
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })         -- Treesitter
+Plug('hrsh7th/cmp-nvim-lsp')                                              -- Completions from LSP
+Plug('hrsh7th/cmp-buffer')                                                -- Completions from buffer names
+Plug('hrsh7th/cmp-path')                                                  -- Completions from file paths
+Plug('hrsh7th/cmp-cmdline')                                               -- Completions from command line
+Plug('hrsh7th/nvim-cmp')                                                  -- nvim completions
+Plug('hrsh7th/cmp-vsnip')                                                 -- Snippets for nvim-cmp
+Plug('hrsh7th/vim-vsnip')                                                 -- Snippets for nvim-cmp
 vim.call('plug#end')
 
 vim.opt.termguicolors = true
@@ -231,19 +238,92 @@ require("nvim-tree").setup {
 		group_empty = true,
 	},
 }
+
+local cmp = require 'cmp'
+
+-- The docs say this should be required, but it's not, from what I can tell
+-- vim.g.copilot_no_tab_map = true
+-- vim.api.nvim_set_keymap('i', '<C-/>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	experimental = {
+		ghost_text = false,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
+
 -- Set up mason, LSP, and lint
 require("mason").setup()
 require("mason-lspconfig").setup {
 	ensure_installed = { "bashls", "eslint", "lua_ls", "pylsp", "pyright", "tsserver", "yamlls", }
 }
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("mason-lspconfig").setup_handlers {
 	-- Automatically configure all LSP servers
 	function(server_name) -- default handler (optional)
-		require("lspconfig")[server_name].setup {}
+		require("lspconfig")[server_name].setup {
+			capabilities = capabilities
+		}
 	end,
 	--- Named blocks for servers that require a specific setup
 	["lua_ls"] = function()
 		require("lspconfig")["lua_ls"].setup {
+			capabilities = capabilities,
 			settings = {
 				Lua = {
 					runtime = {
@@ -265,6 +345,7 @@ require("mason-lspconfig").setup_handlers {
 	end,
 	["pylsp"] = function()
 		require("lspconfig")["pylsp"].setup {
+			capabilities = capabilities,
 			settings = {
 				pylsp = {
 					plugins = {
