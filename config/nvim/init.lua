29d6b1ad3cb2c92c0cce8e5fd9ac0f0e27402927
Mike Miller <github@mikeage.net>
2024-04-09 09:43:49 +0300

    Don't use nvim-cmp on '/' or ':'

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -290,22 +290,22 @@ cmp.setup.filetype('gitcommit', {
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = 'buffer' }
-	}
-})
+--cmp.setup.cmdline({ '/', '?' }, {
+	--mapping = cmp.mapping.preset.cmdline(),
+	--sources = {
+		--{ name = 'buffer' }
+	--}
+--})
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-		{ name = 'cmdline' }
-	})
-})
+--cmp.setup.cmdline(':', {
+	--mapping = cmp.mapping.preset.cmdline(),
+	--sources = cmp.config.sources({
+		--{ name = 'path' }
+	--}, {
+		--{ name = 'cmdline' }
+	--})
+--})
 
 -- Set up mason, LSP, and lint
 require("mason").setup()
