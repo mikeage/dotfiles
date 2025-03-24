e93b614e1e801c8b3272349914022b8e738c749a
Mike Miller <github@mikeage.net>
2025-03-24 10:31:16 +0200

    Add render-markdown, and use it in codecompanion

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -92,6 +92,16 @@ require("lazy").setup({
 		ft = "csv",
 	},
 
+	{
+		'MeanderingProgrammer/render-markdown.nvim',
+		--dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+		ft = { "markdown", "codecompanion" }
+	},
 	-- -------------------------------------------------------------------
 	-- Diff & Utility
 	-- -------------------------------------------------------------------
