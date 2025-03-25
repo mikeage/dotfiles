4b9612c4c0eaf5b0262f576169d3e4c75527d49e
Mike Miller <github@mikeage.net>
2025-03-25 10:37:39 +0200

    Update treesitter config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -537,8 +537,14 @@ require("lazy").setup({
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup {
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "yaml" },
-				highlight = { enable = true },
+				ensure_installed = {
+					"c", "lua", "vim", "vimdoc", "query", "yaml",
+					"python", "javascript", "typescript", "tsx", "html", "css", "json"
+				},
+				auto_install = true,
+				highlight = { enable = true, disable = { "git_rebase" } },
+				indent = { enable = true },
+				incremental_selection = { enable = true },
 			}
 		end,
 	},
