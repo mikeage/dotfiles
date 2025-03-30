6d2eaa54a9117b7461749ba61314d3b4ba212b27
Mike Miller <github@mikeage.net>
2025-03-30 14:29:52 +0300

    Use M-Space instead of C-Space to avoid conflicts with language switching

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -758,7 +758,7 @@ require("lazy").setup({
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"]     = cmp.mapping.scroll_docs(-4),
 					["<C-f>"]     = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+					["<M-Space>"] = cmp.mapping.complete(),
 					["<C-e>"]     = cmp.mapping.abort(),
 					["<CR>"]      = cmp.mapping.confirm({ select = true }),
 				}),
